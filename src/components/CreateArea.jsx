68031eb8fb4fb4ed5bd824d01d36566b2596f2f6
Andrew D Tran <tranandrew.dt@gmail.com>
2024-08-24 15:34:55 -0700

    [init] Project Git Init

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
+import Zoom from "@mui/material/Zoom";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  const [isCreateAreaClicked, setCreateAreaClicked] = useState(false);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
+
+  function handleCreateAreaClick(event) {
+    setCreateAreaClicked(true);
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        {isCreateAreaClicked && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+
+        <textarea
+          name="content"
+          onClick={handleCreateAreaClick}
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isCreateAreaClicked ? "3" : "1"}
+        />
+        <Zoom in={isCreateAreaClicked}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <AcUnitIcon />
+        &nbsp;Notes App
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
